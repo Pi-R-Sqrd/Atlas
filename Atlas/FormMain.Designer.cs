87cc89414f63395345c6c07902d5df90df80ad6a
Pi-R-Sqrd <44962776+Pi-R-Sqrd@users.noreply.github.com>
2019-01-21 13:23:27 +0800

    Added source files via file upload
    Win Form loads an xml file from directory location and inputs the resultant data into a datagrid

---
+++ Atlas/FormMain.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace Atlas
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblInstruction = new System.Windows.Forms.Label();
+            this.btnLoadFile = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.dataGridViewAtlas = new System.Windows.Forms.DataGridView();
+            this.errorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAtlas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblInstruction
+            // 
+            this.lblInstruction.AutoSize = true;
+            this.lblInstruction.Location = new System.Drawing.Point(13, 13);
+            this.lblInstruction.Name = "lblInstruction";
+            this.lblInstruction.Size = new System.Drawing.Size(413, 13);
+            this.lblInstruction.TabIndex = 0;
+            this.lblInstruction.Text = "Click on the \"Load File\" button to lookup and load your xml file into the data gr" +
+    "id below";
+            // 
+            // btnLoadFile
+            // 
+            this.btnLoadFile.Location = new System.Drawing.Point(16, 31);
+            this.btnLoadFile.Name = "btnLoadFile";
+            this.btnLoadFile.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadFile.TabIndex = 1;
+            this.btnLoadFile.Text = "Load File";
+            this.btnLoadFile.UseVisualStyleBackColor = true;
+            this.btnLoadFile.Click += new System.EventHandler(this.btnLoadFile_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog";
+            // 
+            // dataGridViewAtlas
+            // 
+            this.dataGridViewAtlas.AllowUserToAddRows = false;
+            this.dataGridViewAtlas.AllowUserToDeleteRows = false;
+            this.dataGridViewAtlas.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewAtlas.Location = new System.Drawing.Point(16, 60);
+            this.dataGridViewAtlas.Name = "dataGridViewAtlas";
+            this.dataGridViewAtlas.ReadOnly = true;
+            this.dataGridViewAtlas.Size = new System.Drawing.Size(575, 150);
+            this.dataGridViewAtlas.TabIndex = 2;
+            // 
+            // errorProvider
+            // 
+            this.errorProvider.ContainerControl = this;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 266);
+            this.Controls.Add(this.dataGridViewAtlas);
+            this.Controls.Add(this.btnLoadFile);
+            this.Controls.Add(this.lblInstruction);
+            this.Name = "FormMain";
+            this.Text = "Atlas";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewAtlas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstruction;
+        private System.Windows.Forms.Button btnLoadFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.DataGridView dataGridViewAtlas;
+        private System.Windows.Forms.ErrorProvider errorProvider;
+    }
+}
+
---
+++ Atlas/FormMain.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Atlas
+{
+    public partial class FormMain : Form
+    {
+        private readonly string[] _xmlMimeType = new string[] { "text/xml", "application/xml"};
+       
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoadFile_Click(object sender, EventArgs e)
+        {
+            errorProvider.Clear();
+            
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if (!IsXMLFile(openFileDialog.SafeFileName))
+                    {
+                        throw new Exception("The selected file must be a .xml file");
+                    }
+
+                    PopulateDataGrid();
+                }
+                catch (Exception ex)
+                {
+                    errorProvider.SetError(btnLoadFile, ex.Message);
+                }
+            }
+        }
+
+        private bool IsXMLFile(string fileName)
+        {
+            var mimetype = GetMimeType(fileName);
+
+            return _xmlMimeType.Contains(mimetype);
+        }
+
+        private string GetMimeType(string fileName)
+        {
+            string mimeType = "application/unknown";
+            string ext = System.IO.Path.GetExtension(fileName).ToLower();
+            Microsoft.Win32.RegistryKey regKey = Microsoft.Win32.Registry.ClassesRoot.OpenSubKey(ext);
+            if (regKey != null && regKey.GetValue("Content Type") != null)
+                mimeType = regKey.GetValue("Content Type").ToString();
+            return mimeType;
+        }
+
+        private void PopulateDataGrid()
+        {
+            var atlasXML = XElement.Load(openFileDialog.FileName);
+
+            var query = from xmlCountry in atlasXML.Descendants("country")
+
+                        select new
+                        {
+                            Country = xmlCountry.Attribute("name").Value,
+                            City = xmlCountry.Element("city").Attribute("name").Value,
+                            Language = xmlCountry.Attribute("languagespoken").Value,
+                            Attraction = xmlCountry.Element("city").Attribute("attraction").Value
+                        };
+
+            dataGridViewAtlas.DataSource = query.ToList();
+
+        }
+    }
+}
